7d098814b6f772b0b9f251f6c0a7b55a7113b018
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-03-11 15:03:23 +0300

    style: add validation to add new issue form

--- src/app/issue-report/issue-report.component.ts
+++ src/app/issue-report/issue-report.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Issue } from '../issue';
 import { IssuesService } from '../issues.service';
 
@@ -11,21 +11,26 @@ import { IssuesService } from '../issues.service';
 export class IssueReportComponent {
   issueForm: FormGroup | undefined;
   suggestions: Issue[] = [];
-  
+
   constructor(private builder: FormBuilder, private issueService: IssuesService) { }
   
   @Output() formClose = new EventEmitter();
   
   ngOnInit(): void {
     this.issueForm = this.builder.group({
-      title: [''],
+      title: ['', Validators.required],
       description: [''],
-      priority: [''],
-      type: ['']
+      priority: ['', Validators.required],
+      type: ['', Validators.required]
     });
   }
 
   addIssue() {
+    if (this.issueForm && this.issueForm.invalid) {
+      this.issueForm.markAllAsTouched();
+      return;
+    }
+  
     this.issueService.createIssue(this.issueForm?.value);
     this.formClose.emit();
   }
